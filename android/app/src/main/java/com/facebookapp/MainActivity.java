7a1a5aa1bc95647e51554014523c0999ebf2043f
Arwa1991 <arwa.louihig@esprit.tn>
2016-11-21 20:36:24 +0100

    commit

--- android/app/src/main/java/com/facebookapp/MainActivity.java
+++ android/app/src/main/java/com/facebookapp/MainActivity.java
@@ -1,23 +1,9 @@
 package com.facebookapp;
 
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import com.facebook.FacebookSdk;
-import com.facebook.CallbackManager;
-
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.appevents.AppEventsLogger;
-
-import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-    CallbackManager mCallbackManager =
-            MainApplication.getCallbackManager();
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -26,18 +12,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "FacebookApp";
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-		SplashScreen.show(this);  // here
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-    }
-
 }
