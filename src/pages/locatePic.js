9daed4c0341a93f230733ef573deef66f6bcd71e
Arwa1991 <arwa.louihig@esprit.tn>
2016-11-30 13:58:11 +0100

    commiting chages in location interface

--- src/pages/locatePic.js
+++ src/pages/locatePic.js
@@ -14,7 +14,7 @@ import {
   Image,
   TextInput,
   BackAndroid,
-  TouchableHighlight,
+  TouchableOpacity,
   View
 } from 'react-native';
 import HeaderUp from '../components/headerUp';
@@ -37,57 +37,63 @@ export default class locatePic extends Component {
 	}
 	localiser_tete(){
 		alert("Tête");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        }) */; 
 	}
 	localiser_cou(){
 		alert("Cou");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
 	}
 	localiser_epaule_gauche(){
 		alert("Epaule Gauche");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
 	}
 	localiser_epaule_droite(){
 		alert("Epaule Droite");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
 	}
-	localiser_tronc(){
-		alert("Tronc");
-		this.props.navigator.push({
+	localiser_thorax(){
+		alert("Thorax");
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
+	}
+	localiser_abdomen(){
+		alert("Abdomen");
+/* 		this.props.navigator.push({
+          component: TakePicture
+        });  */
 	}
 	localiser_main_gauche(){
 		alert("Main Gauche");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
 	}
 	localiser_main_droite(){
 		alert("Main Droite");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        }); */ 
 	}
 	localiser_jambe_gauche(){
 		alert("Jambe Gauche");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        }); */ 
 	}
 	localiser_jambe_droite(){
 		alert("Jambe Droite");
-		this.props.navigator.push({
+/* 		this.props.navigator.push({
           component: TakePicture
-        }); 
+        });  */
 	}
 	vue_arriere(){
 		alert("C'est la vue de face");
@@ -109,56 +115,38 @@ export default class locatePic extends Component {
 				<Button style={{backgroundColor:'transparent',marginLeft: 0,marginTop:0,width:180,height:60,alignItems: 'flex-start',borderColor:'#8684a3'}} textStyle={{color:'#8684a3'}} rounded bordered>Avant</Button>
 				<Button style={{backgroundColor:'transparent',marginTop:0,width:180,height:60,alignItems: 'flex-start',borderColor:'#8684a3'}} textStyle={{color:'#fff'}} rounded bordered>Arriére</Button>
 			</View>
-			<ButtonS
-				text="Tête"
-				onpress={this.localiser_tete.bind(this)}
-				button_styles={{top:27}}
-				button_text_styles={{textAlign:'left',color:'#53507c',left:75}}/>
-			<ButtonS
-				text="Cou"
-				onpress={this.localiser_cou.bind(this)}
-				button_styles={{top:65}}
-				button_text_styles={{textAlign:'right',color:'#53507c',right:25}}/>
-			<ButtonS
-				text="Epaule Gauche"
-				onpress={this.localiser_epaule_gauche.bind(this)}
-				button_styles={{top:50}}
-				button_text_styles={{textAlign:'right',color:'#53507c',right:5}}/>
-			<ButtonS
-				text="Epaule Droite"
-				onpress={this.localiser_epaule_droite.bind(this)}
-				button_styles={{top:30}}
-				button_text_styles={{textAlign:'left',color:'#53507c',left:5}}/>
-			<ButtonS
-				text="Thorax"
-				onpress={this.localiser_tronc.bind(this)}
-				button_styles={{top:80}}
-				button_text_styles={{textAlign:'left',color:'#53507c',left:35}}/>
-			<ButtonS
-				text="Abdomen"
-				onpress={this.localiser_tronc.bind(this)}
-				button_styles={{top:80}}
-				button_text_styles={{textAlign:'right',color:'#53507c',right:35}}/>
-			<ButtonS
-				text="Main Gauche"
-				onpress={this.localiser_main_gauche.bind(this)}
-				button_styles={{top:150}}
-				button_text_styles={{textAlign:'right',color:'#53507c',right:5}}/>
-			<ButtonS
-				text="Main Droite"
-				onpress={this.localiser_main_droite.bind(this)}
-				button_styles={{top:130}}
-				button_text_styles={{textAlign:'left',color:'#53507c',left:5}}/>
-			<ButtonS
-				text="Jambe Gauche"
-				onpress={this.localiser_jambe_gauche.bind(this)}
-				button_styles={{top:180}}
-				button_text_styles={{textAlign:'right',color:'#53507c',right:38}}/>
-			<ButtonS
-				text="Jambe Droite"
-				onpress={this.localiser_jambe_droite.bind(this)}
-				button_styles={{top:160}}
-				button_text_styles={{textAlign:'left',color:'#53507c',left:38}}/>
+			<View style={{flexDirection: 'column',backgroundColor: 'transparent'}}>
+			<TouchableOpacity
+				onPress={this.localiser_tete.bind(this)}
+				style={{width:200,height:20,marginLeft:115,marginTop:28,marginBottom:30}}><Text>Tête</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_cou.bind(this)}
+				style={{width:200,height:20,marginLeft:220}}><Text>Cou</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_epaule_gauche.bind(this)}
+				style={{width:200,height:20,marginLeft:250}}><Text>Epaule Gauche</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_epaule_droite.bind(this)}
+				style={{width:200,height:20,marginLeft:5,marginBottom:10}}><Text>Epaule Droite</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_thorax.bind(this)}
+				style={{width:200,height:20,marginLeft:50,marginBottom:10}}><Text>Thorax</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_abdomen.bind(this)}
+				style={{width:200,height:20,marginLeft:180,marginBottom:10}}><Text>Abdomen</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_main_gauche.bind(this)}
+				style={{width:200,height:20,marginLeft:180,marginBottom:10}}><Text>Main Gauche</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_main_droite.bind(this)}
+				style={{width:200,height:20,marginLeft:50,marginBottom:40}}><Text>Main Droite</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_jambe_gauche.bind(this)}
+				style={{width:200,height:20,marginLeft:185,marginBottom:10}}><Text>Jambe Gauche</Text></TouchableOpacity>
+			<TouchableOpacity
+				onPress={this.localiser_jambe_droite.bind(this)}
+				style={{width:200,height:20,marginLeft:50,marginBottom:10}}><Text>Jambe Droite</Text></TouchableOpacity>
+			</View>
 		</Image>
 	</ScrollView>
      </View>
