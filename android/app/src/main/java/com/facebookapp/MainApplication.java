8e3295ab5b63cacf7d59be96b21174b19040653e
walid <walid@osereso.fr>
2017-01-24 10:29:15 +0100

    commit4

--- android/app/src/main/java/com/facebookapp/MainApplication.java
+++ android/app/src/main/java/com/facebookapp/MainApplication.java
@@ -40,8 +40,8 @@ public class MainApplication extends Application implements ReactApplication {
             protected List<ReactPackage> getPackages() {
               return Arrays.<ReactPackage>asList(
 						new MainReactPackage(),
+            new RCTCameraPackage(),
 						new RNFetchBlobPackage(),
-						new RCTCameraPackage(),
 						new FBSDKPackage(mCallbackManager),
 						new VectorIconsPackage()
               );
--- src/pages/gestionPatient.js
+++ src/pages/gestionPatient.js
@@ -22,10 +22,11 @@ import styles from '../styles/common-styles.js';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import {InputGroup, Input, Button, Card, CardItem, List, ListItem} from 'native-base';
 import HeaderUp from '../components/headerUp';
-import NewPatientDynamic from './newPatientDynamic';
+import UploadForm from './uploadForm';
 import NewPatient from './newPatient';
 import LocatePic from './locatePic';
 import RechercheP from './rechercheP';
+import LastOne from './lastOne';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 const window = Dimensions.get('window');
 export default class gestionPatient extends Component {
@@ -35,27 +36,26 @@ export default class gestionPatient extends Component {
 		  loaded: true,
           id: ''
 		}
-		this.ajoutPat = this.ajoutPat.bind(this);
-		this.modPat = this.modPat.bind(this);
-		this.goBack = this.goBack.bind(this);
 	}
 
 	ajoutPat(){
-		AsyncStorage.getItem('id').then((id_categorie) => {
-		if(id_categorie=="naevus"){
 		this.props.navigator.push({
 			component: NewPatient
-		});
-		}else{
-			this.props.navigator.push({
-			component: NewPatientDynamic
-		});
-		}
 		});
 	}
 	modPat(){
 		this.props.navigator.push({
           component: RechercheP
+        }); 
+	}
+	last(){
+		this.props.navigator.push({
+          component: LastOne
+        }); 
+	}
+	upl(){
+		this.props.navigator.push({
+          component: UploadForm
         }); 
 	}	
 	goBack() {
@@ -65,23 +65,23 @@ export default class gestionPatient extends Component {
   render() {
     return (
 	<View>
-	<HeaderUp text="   Gestion des patients" loaded={true} onpress={this.goBack}/>
+	<HeaderUp text="   Gestion des patients" loaded={true} onpress={this.goBack.bind(this)}/>
 		 <View style={{margin:7, marginTop:30}}>
 		 <List>
 		 <ListItem style={{height:(window.height/2)-70}}>
 		   <Grid>
 		   <Row>
-		      <Col style={{width:30,height:100, marginTop:5}}><Icon name="plus-circle" style={{color:'#29235c', fontSize:20}} /></Col>
+		      <Col style={{width:30,height:50, marginTop:5}}><Icon name="plus-circle" style={{color:'#29235c', fontSize:20}} /></Col>
 			    <Col>
 					<Text style={{width:200, fontFamily: 'Roboto', fontSize:18, color:'#29235c'}}>Ajouter un patient{"\n"}</Text>
 				</Col>	
 			</Row>
-			<Row style={{height:30}}>
+			<Row style={{height:70, marginBottom:30}}>
 				<Text style={{width:320, fontFamily: 'Roboto', fontSize:14, color:'#29235c'}}>Si vous voulez ajouter un patient il suffit de cliquer sur le bouton "AJOUTER UN PATIENT". Il vous suffira d'inserer les informations de votre patient.</Text>
 			</Row>
 			<Row>
 				<Button
-				onPress={this.ajoutPat}
+				onPress={this.ajoutPat.bind(this)}
 				style={styles.primary_button_ajout_patient}
 				textStyle={styles.primary_button_text_ajout_patient}>AJOUTER UN NOUVEAU PATIENT</Button>
 			</Row>
@@ -89,22 +89,22 @@ export default class gestionPatient extends Component {
 		 </ListItem>
 		  </List>	
 	  </View>	
-	   <View style={{margin:7, marginTop:15}}>
+	   <View style={{margin:7, marginBottom:35}}>
 		 <List>
 		 <ListItem style={{height:(window.height/2)-70}}>
 		   <Grid>
 		   <Row>
-		      <Col style={{width:30,height:100, marginTop:5}}><Icon name="edit" style={{color:'#29235c', fontSize:20}}  /></Col>
+		      <Col style={{width:30,height:50}}><Icon name="edit" style={{color:'#29235c', fontSize:20}}  /></Col>
 			    <Col>
 					<Text style={{width:250, fontFamily: 'Roboto', fontSize:18, color:'#29235c'}}>Modifier la fiche patient{"\n"}</Text>
 				</Col>
 			</Row>	
-			<Row style={{height:40}}>
+			<Row style={{height:75, marginBottom:30}}>
 			<Text style={{width:320, fontFamily: 'Roboto', fontSize:14, color:'#29235c'}}>Si vous voulez modifier la fiche du patient il suffit de cliquer sur le bouton "MODIFIER UN PATIENT". Il vous suffira de modifier les informations concernant votre patient.</Text>
 			</Row>
 			<Row>
 				<Button
-				onPress={this.modPat}
+				onPress={this.modPat.bind(this)}
 				style={styles.primary_button_modifier_patient}
 				textStyle={styles.primary_button_text_ajout_patient}>MODIFIER LA FICHE PATIENT</Button>
 			</Row>
