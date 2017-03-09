740d6b053f9f8b9d122cb1cf7f8a73f624b6e3b6
Arwa1991 <arwa.louihig@esprit.tn>
2017-03-09 09:41:58 +0100

    deleting alerts and wsarnings

--- src/pages/phototype.js
+++ src/pages/phototype.js
@@ -56,157 +56,155 @@ export default class phototype extends Component {
 	this.goBack=this.goBack.bind(this);
   }
   phototype_1(){
-	    AsyncStorage.setItem('Phototype_value','I');
-	    AsyncStorage.setItem('Sed_Value','2.5');
-		this.setState({back_color: '#322a7d', textColor: 'white',text2: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
-	phototype_2(){
-		AsyncStorage.setItem('Phototype_value','II');
-		AsyncStorage.setItem('Sed_Value','3.0');
-		this.setState({back_color1: '#322a7d', textColor1: 'white',text3: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
-	phototype_3(){
-		AsyncStorage.setItem('Phototype_value','III');
-		AsyncStorage.setItem('Sed_Value','4.5');
-		this.setState({back_color2: '#322a7d', textColor2: 'white',text4: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
-	phototype_4(){
-		AsyncStorage.setItem('Phototype_value','IV');
-		AsyncStorage.setItem('Sed_Value','6.0');
-		this.setState({back_color3: '#322a7d', textColor3: 'white',text5: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
-	phototype_5(){
-		AsyncStorage.setItem('Phototype_value','V');
-		AsyncStorage.setItem('Sed_Value','7.5');
-		this.setState({back_color4: '#322a7d', textColor4: 'white',text6: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
-	phototype_6(){
-		AsyncStorage.setItem('Phototype_value','VI');
-		AsyncStorage.setItem('Sed_Value','12.0');
-		this.setState({back_color5: '#322a7d', textColor5: 'white',text7: 'white'});
-		this.props.navigator.push({
-          component: UploadForm
-        }); 
-	}
+	AsyncStorage.setItem('Phototype_value','I');
+	AsyncStorage.setItem('Sed_Value','2.5');
+	this.setState({back_color: '#322a7d', textColor: 'white',text2: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
+  phototype_2(){
+	AsyncStorage.setItem('Phototype_value','II');
+	AsyncStorage.setItem('Sed_Value','3.0');
+	this.setState({back_color1: '#322a7d', textColor1: 'white',text3: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
+  phototype_3(){
+	AsyncStorage.setItem('Phototype_value','III');
+	AsyncStorage.setItem('Sed_Value','4.5');
+	this.setState({back_color2: '#322a7d', textColor2: 'white',text4: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
+  phototype_4(){
+	AsyncStorage.setItem('Phototype_value','IV');
+	AsyncStorage.setItem('Sed_Value','6.0');
+	this.setState({back_color3: '#322a7d', textColor3: 'white',text5: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
+  phototype_5(){
+	AsyncStorage.setItem('Phototype_value','V');
+	AsyncStorage.setItem('Sed_Value','7.5');
+	this.setState({back_color4: '#322a7d', textColor4: 'white',text6: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
+  phototype_6(){
+	AsyncStorage.setItem('Phototype_value','VI');
+	AsyncStorage.setItem('Sed_Value','12.0');
+	this.setState({back_color5: '#322a7d', textColor5: 'white',text7: 'white'});
+	this.props.navigator.push({
+		component: UploadForm
+	}); 
+  }
   goBack() {
-		this.props.navigator.pop();
-		return true; 
-	}
+	this.props.navigator.pop();
+	return true; 
+  }
   render() {
     return ( 
 	<View>
 	  <HeaderUp text="Phototype" loaded={this.state.loaded} onpress={this.goBack}/>
-		<ScrollView>	
-		  <List>
-	        <ListItem  style={styles.list_Phototype}>
-			   <Grid> 
-				    <Button onPress={this.phototype_1} style={{backgroundColor: this.state.back_color, borderColor:'black', width: 420,height:90}} textStyle={{color: this.state.textColor}} transparent>
-					     <Row style={{width: 400,height:90}} >
-								<Col style={{width: 75}}>
-									<Image style={styles.circle} source={{uri:'http://localhost:8081/img/1.jpg'}}/>
-								</Col>
-								<Col style={{width: 325}}>
-									<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor}}>Phototypes I{"\n"}</Text> 
-									<Text style={{fontSize: 13,color: this.state.text2}}>Peau trés blanche, yeux clairs,cheveux{"\n"}roux/blonds,taches de rousseur ++</Text>
-								</Col>
-						</Row>		
-						
-			         </Button>			 
-               </Grid>				 
-			</ListItem>  
-			 <ListItem style={styles.list_Phototype}>
-			  <Grid> 
-				 <Button onPress={this.phototype_2} style={{backgroundColor: this.state.back_color1, width: 420,height:90}} textStyle={{color: this.state.textColor1}} transparent>
-						 <Row style={{width: 400,height:90}} >
-							<Col style={{width:75}}>
-								<Image style={styles.circle} source={{uri:'http://localhost:8081/img/2.jpg'}}/>
-							</Col> 
-							<Col style={{width: 325}}>
-								<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor1}}>Phototypes II{"\n"}</Text> 
-								<Text style={{fontSize: 13,color: this.state.text3}}>Peau claire, yeux clairs,cheveux blonds/chatains,{"\n"}taches de rousseur +</Text>
-							</Col>
-						 </Row>	
-			     </Button>			 
-               </Grid>				 
-			 </ListItem>
-			 <ListItem  style={styles.list_Phototype}>
-			   <Grid> 
-				 <Button onPress={this.phototype_3} style={{backgroundColor: this.state.back_color2, width: 420,height:90}} textStyle={{color: this.state.textColor2}} transparent>
-					  <Row style={{width: 400,height:90}} >
-							<Col style={{width:75}}>
-								<Image style={styles.circle} source={{uri:'http://localhost:8081/img/3.jpg'}}/>
-							</Col>
-							<Col style={{width: 325}}>
-								<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor2}}>Phototypes III{"\n"}</Text> 
-								<Text style={{fontSize: 13,color: this.state.text4}}>Peau intermédiaire, yeux bruns,{"\n"}cheveux chatains</Text>
-							</Col>
-						 </Row>	
-			     </Button>			 
-               </Grid>				 
-			</ListItem>
-			<ListItem  style={styles.list_Phototype}>
-			   <Grid> 
-				 <Button onPress={this.phototype_4} style={{backgroundColor: this.state.back_color3,width: 420,height:90}} textStyle={{color: this.state.textColor3}} transparent>
-						 <Row style={{width: 400,height:90}} >
-							<Col style={{width:75}}>
-								<Image style={styles.circle} source={{uri:'http://localhost:8081/img/4.jpg'}}/>
-							</Col>
-							<Col style={{width: 325}}>
-								<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor3}}>Phototypes IV{"\n"}</Text> 
-								<Text style={{fontSize: 13,color: this.state.text5}}>Peau mate, yeux bruns/noirs,{"\n"}cheveux bruns/noirs</Text>
-							</Col>
-						 </Row>	
-			     </Button>			 
-               </Grid>				 
-			</ListItem>
-			<ListItem  style={styles.list_Phototype}>
-			   <Grid> 
-				 <Button onPress={this.phototype_5} style={{backgroundColor: this.state.back_color4, width: 420,height:90}} textStyle={{color: this.state.textColor4}} transparent>
-						 <Row style={{width: 400,height:90}} >
-							<Col style={{width:75}}>
-								<Image style={styles.circle} source={{uri:'http://localhost:8081/img/5.jpg'}}/>
-							</Col>
-							<Col style={{width: 325}}>
-								<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor4}}>Phototypes V{"\n"}</Text> 
-								<Text style={{fontSize: 13,color: this.state.text6}}>Peau brune foncée, yeux noirs,{"\n"}cheveux noirs</Text>
-							</Col>
-						 </Row>	
-			     </Button>			 
-               </Grid>				 
-			</ListItem>
-			<ListItem  style={styles.list_Phototype}>
-			   <Grid> 
-				 <Button onPress={this.phototype_6} style={{backgroundColor: this.state.back_color5,width: 420,height:90}} textStyle={{color: this.state.textColor5}} transparent>
-						 <Row style={{width: 400,height:90}} >
-							<Col style={{width:75}}>
-								<Image style={styles.circle} source={{uri:'http://localhost:8081/img/6.jpg'}}/>
-							</Col>
-							<Col style={{width: 325}}>
-								<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor5}}>Phototypes VI{"\n"}</Text> 
-								<Text style={{fontSize: 13,color: this.state.text7}}>Peau noire, yeux noirs,cheveux noirs.</Text> 
-							</Col>
-						 </Row>	
-			     </Button>			 
-               </Grid>				 
-			</ListItem>
-		  </List>
-		  
-		</ScrollView>      
+	  <ScrollView>	
+		<List>
+		  <ListItem  style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_1} style={{backgroundColor: this.state.back_color, borderColor:'black', width: 420,height:90}} textStyle={{color: this.state.textColor}} transparent>
+				<Row style={{width: 400,height:90}} >
+				  <Col style={{width: 75}}>
+					<Image style={styles.circle} source={{uri:'http://localhost:8081/img/1.jpg'}}/>
+				  </Col>
+				  <Col style={{width: 325}}>
+					<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor}}>Phototypes I{"\n"}</Text> 
+					<Text style={{fontSize: 13,color: this.state.text2}}>Peau trés blanche, yeux clairs,cheveux{"\n"}roux/blonds,taches de rousseur ++</Text>
+				  </Col>
+				</Row>				
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>  
+		  <ListItem style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_2} style={{backgroundColor: this.state.back_color1, width: 420,height:90}} textStyle={{color: this.state.textColor1}} transparent>
+				<Row style={{width: 400,height:90}} >
+					<Col style={{width:75}}>
+						<Image style={styles.circle} source={{uri:'http://localhost:8081/img/2.jpg'}}/>
+					</Col> 
+					<Col style={{width: 325}}>
+						<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor1}}>Phototypes II{"\n"}</Text> 
+						<Text style={{fontSize: 13,color: this.state.text3}}>Peau claire, yeux clairs,cheveux blonds/chatains,{"\n"}taches de rousseur +</Text>
+					</Col>
+				</Row>	
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>
+		  <ListItem  style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_3} style={{backgroundColor: this.state.back_color2, width: 420,height:90}} textStyle={{color: this.state.textColor2}} transparent>
+				<Row style={{width: 400,height:90}} >
+				  <Col style={{width:75}}>
+					<Image style={styles.circle} source={{uri:'http://localhost:8081/img/3.jpg'}}/>
+				  </Col>
+				  <Col style={{width: 325}}>
+					<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor2}}>Phototypes III{"\n"}</Text> 
+					<Text style={{fontSize: 13,color: this.state.text4}}>Peau intermédiaire, yeux bruns,{"\n"}cheveux chatains</Text>
+				  </Col>
+				</Row>	
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>
+		  <ListItem  style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_4} style={{backgroundColor: this.state.back_color3,width: 420,height:90}} textStyle={{color: this.state.textColor3}} transparent>
+				<Row style={{width: 400,height:90}} >
+				  <Col style={{width:75}}>
+					<Image style={styles.circle} source={{uri:'http://localhost:8081/img/4.jpg'}}/>
+				  </Col>
+				  <Col style={{width: 325}}>
+					<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor3}}>Phototypes IV{"\n"}</Text> 
+					<Text style={{fontSize: 13,color: this.state.text5}}>Peau mate, yeux bruns/noirs,{"\n"}cheveux bruns/noirs</Text>
+				  </Col>
+				</Row>	
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>
+		  <ListItem  style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_5} style={{backgroundColor: this.state.back_color4, width: 420,height:90}} textStyle={{color: this.state.textColor4}} transparent>
+				<Row style={{width: 400,height:90}} >
+				  <Col style={{width:75}}>
+					<Image style={styles.circle} source={{uri:'http://localhost:8081/img/5.jpg'}}/>
+				  </Col>
+				  <Col style={{width: 325}}>
+					<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor4}}>Phototypes V{"\n"}</Text> 
+					<Text style={{fontSize: 13,color: this.state.text6}}>Peau brune foncée, yeux noirs,{"\n"}cheveux noirs</Text>
+				  </Col>
+				</Row>	
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>
+		  <ListItem  style={styles.list_Phototype}>
+			<Grid> 
+			  <Button onPress={this.phototype_6} style={{backgroundColor: this.state.back_color5,width: 420,height:90}} textStyle={{color: this.state.textColor5}} transparent>
+				<Row style={{width: 400,height:90}} >
+				  <Col style={{width:75}}>
+					<Image style={styles.circle} source={{uri:'http://localhost:8081/img/6.jpg'}}/>
+				  </Col>
+				  <Col style={{width: 325}}>
+					<Text style={{fontSize: 15,fontWeight:"bold",color: this.state.textColor5}}>Phototypes VI{"\n"}</Text> 
+					<Text style={{fontSize: 13,color: this.state.text7}}>Peau noire, yeux noirs,cheveux noirs.</Text> 
+				  </Col>
+				</Row>	
+			  </Button>			 
+			</Grid>				 
+		  </ListItem>
+		</List>
+	  </ScrollView>      
 	</View>
     );
   }
